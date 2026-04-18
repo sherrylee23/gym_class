558781e433df75955b6954d91a6aae1b8b27bdd0
sherry lee <sherrylee2317@gmail.com>
2026-04-19 01:32:41 +0800

    different the factory file

--- Model/Schedule.php
+++ Model/Schedule.php
@@ -1,33 +1,7 @@
 <?php
 
 require_once('Database.php');
-
-// Design Patter: Factory Pattern --> decouples the logic of class types from the main schdeule class
-abstract class SessionAccess {
-
-    public abstract function getLabel();
-}
-
-class PremiumAccess extends SessionAccess {
-
-    public function getLabel() {
-        return "Members Only";
-    }
-}
-
-class FreeAccess extends SessionAccess {
-
-    public function getLabel() {
-        return "Open to Public";
-    }
-}
-
-class AccessFactory {
-
-    public static function create($isFree) {
-        return $isFree ? new FreeAccess() : new PremiumAccess();
-    }
-}
+require_once('AccessFactory.php');
 
 class Schedule {
 
